800512a4f78243bb5a2710131b69d0ec84213c71
sakshi-walunjkar <sakshiwalunjkar4@gmail.com>
2026-04-19 15:18:35 +0530

    Added LeetCode solutions with README

---
+++ Easy/Concatenation_of_Array_1929.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Concatenation_of_Array_1929 {
+
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+
+        for(int i = 0; i < n; i++){
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Take size of array
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        // Take array input
+        System.out.println("Enter elements:");
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
+
+        Concatenation_of_Array_1929 obj = new Concatenation_of_Array_1929();
+        int[] result = obj.getConcatenation(nums);
+
+        // Print result
+        System.out.println("Concatenated array:");
+        for(int num : result){
+            System.out.print(num + " ");
+        }
+
+        sc.close();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+This repository contains my solutions to LeetCode problems using Java. The purpose of this repository is to improve problem-solving skills, build strong data structures and algorithms concepts, and prepare for technical interviews.
+
